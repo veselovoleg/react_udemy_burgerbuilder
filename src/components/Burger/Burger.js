d7a060e7d28707fe06e24c2302d9e434256d9029
olegveselov <oleg@loftdigital.com>
2021-03-16 21:46:35 +0100

    Added controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(key => [...Array(props.ingredients[key]).fill(key)])
+    .flat()
+    .map((key, index) => <BurgerIngredient type={key} key={index + '_' + key} />);
+  
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients.length ? transformedIngredients : <p>Please add some ingredients</p>}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 export default class BurgerBuilder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+      }
+    };
+  }
+
+  state;
+
   render() {
     return (
       <Aux>
-        <Burger></Burger>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients}></Burger>
+        <BuildControls />
       </Aux>
     );
   }
